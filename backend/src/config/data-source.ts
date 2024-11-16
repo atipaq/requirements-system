cd946b6bd4f83ca1c2666d9e18b4f4c612e1d4d0
Alex Huaracha <alex.ronny.hq@gmail.com>
2024-11-16 16:15:18 -0500

    Updating database paths in data-source

--- backend/src/config/data-source.ts
+++ backend/src/config/data-source.ts
@@ -7,7 +7,7 @@ dotenv.config();
 
 export const AppDataSource = new DataSource({
     type: 'sqlite',
-    database: process.env.DATABASE_PATH || './data/database.sqlite',
+    database: process.env.DATABASE_PATH || './storage/database.sqlite',
     synchronize: true, // Usa migraciones en lugar de sincronización automática
     logging: false,
     entities: ['src/data/entities/**/*.ts'],
