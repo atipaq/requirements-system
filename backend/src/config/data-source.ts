b421f207af2f94f8525c20134929bd5c88303abb
Alex Huaracha <alex.ronny.hq@gmail.com>
2024-11-16 16:08:50 -0500

    Updating config data-source

--- backend/src/config/data-source.ts
+++ backend/src/config/data-source.ts
@@ -1,12 +1,13 @@
 //backend/src/config/data-source.ts
 import "reflect-metadata"
 import { DataSource } from 'typeorm';
-// Importar entidades o modelos
+import dotenv from 'dotenv';
 
+dotenv.config();
 
 export const AppDataSource = new DataSource({
     type: 'sqlite',
-    database: './database.sqlite',
+    database: process.env.DATABASE_PATH || './data/database.sqlite',
     synchronize: true, // Usa migraciones en lugar de sincronización automática
     logging: false,
     entities: ['src/data/entities/**/*.ts'],
